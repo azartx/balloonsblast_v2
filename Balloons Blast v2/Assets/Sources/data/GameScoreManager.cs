b8658bbb002fd748a60742500923998ab825c098
Solo4 <akasha02@mail.ru>
2025-05-18 20:26:40 +0300

    add todo

--- Balloons Blast v2/Assets/Sources/data/GameScoreManager.cs
+++ Balloons Blast v2/Assets/Sources/data/GameScoreManager.cs
@@ -4,6 +4,8 @@ using System.Collections;
 using TMPro;
 using Game;
 
+//TODO: складывать счет в рамках игры в список (на случай если будет несколько игр за одну сессию (когда игрок в конце раунда нажимает на играть сначала
+// и по выходу забирать из списка лучший счет. список после этого чистить
 public static class GameScoreManager
 {
     private static readonly int DEF_SCORE_UP = 100;
